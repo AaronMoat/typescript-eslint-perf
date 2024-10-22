5ed58c0017a1fc0afee623bf9a59a54ac1c79e45
Aaron Moat <2937187+AaronMoat@users.noreply.github.com>
2024-10-22 20:39:40 +1100

    Downgrade to v7

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -9,8 +9,7 @@ module.exports = [
       ...tseslint.configs.base.languageOptions,
       parserOptions: {
         ...(tseslint.configs.base.languageOptions?.parserOptions ?? {}),
-        projectService: true,
-        // project: "./tsconfig.json",
+        project: "./tsconfig.json",
       },
     },
 
